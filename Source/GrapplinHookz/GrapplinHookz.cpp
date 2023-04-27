cd1502dea5457b08fa7e41461eff494506d75730
blaflamme87 <34461430+blaflamme87@users.noreply.github.com>
2023-04-27 16:05:29 -0400

    added Cpp class

---
+++ Source/GrapplinHookz/GrapplinHookz.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GrapplinHookz.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GrapplinHookz, "GrapplinHookz" );
